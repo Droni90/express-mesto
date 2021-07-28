b7c4b7bb5375292bc0f4b22c3490e98e4cbaa905
Andrey Botygin <dron-i90@yandex.ru>
2021-07-29 00:04:48 +0300

    fx

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
+const { celebrate, Joi } = require("celebrate");
 const { createUser, login } = require("./controllers/users");
+const auth = require("./middlewares/auth");
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -17,11 +19,19 @@ mongoose.connect("mongodb://localhost:27017/mestodb", {
   useUnifiedTopology: true,
 });
 
-app.use("/", require("./routes/user"));
-app.use("/", require("./routes/card"));
-
+app.post("/signup", celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(8).max(35),
+  }),
+}), createUser);
 app.post("/signin", login);
-app.post("/signup", createUser);
+
+app.use("/users", auth, require("./routes/user"));
+app.use("/cards", auth, require("./routes/card"));
 
 app.use("*", (req, res) => {
   res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
